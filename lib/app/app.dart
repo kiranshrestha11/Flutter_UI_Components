e6de92416874d6c37409a0786cf0366ddf7ebd3e
Surat Adhikari <surajadkhari@gmail.com>
2022-08-24 10:15:57 +0545

    commit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,5 @@
-import 'package:ecommercelayout/app/home/presentation/dashboard.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dashboard/presentation/dashboard.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
--- lib/app/dashboard/presentation/dashboard.dart
+++ lib/app/dashboard/presentation/dashboard.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [],
+        ),
+      ),
+    );
   }
 }
